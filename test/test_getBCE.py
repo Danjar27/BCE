e1e1c4f1052efee829f9772604f6d74ca2ed181e
Daniel Jaramillo <steven.villalobos@devsu.com>
2022-04-07 23:43:38 -0500

    changed tests

--- test/test_getBCE.py
+++ test/test_getBCE.py
@@ -1,7 +1,5 @@
 import unittest
 
-import pandas.core.frame
-
 from getBCE.menu import *
 
 
@@ -9,7 +7,7 @@ def query():
     return getBCE('2020', 'enero', '050')
 
 
-class MyTestCase(unittest.TestCase):
+class BCE(unittest.TestCase):
 
     def test_if_returns_string(self):
         self.assertIsInstance(query().href, str)
@@ -20,11 +18,6 @@ class MyTestCase(unittest.TestCase):
 
     def test_if_table_begins_with_none(self):
         self.assertEqual(str(type(query().table)), "<class 'NoneType'>")
-
-    def test_if_create_table_works(self):
-        table = getBCE('2020', 'enero', '050')
-        table.createTable()
-        self.assertIsInstance(table.table, pandas.core.frame.DataFrame)
 
 
 if __name__ == '__main__':
