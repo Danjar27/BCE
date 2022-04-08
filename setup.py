a04db51441fc6aac0761e6d7ecf7f7d8723ddf49
Daniel Jaramillo <steven.villalobos@devsu.com>
2022-04-08 00:08:49 -0500

    refactored setup to automatize deployment

--- setup.py
+++ setup.py
@@ -1,9 +1,32 @@
-from distutils.core import setup
+import setuptools
+import subprocess
+import os
 
-setup(
+remote_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+if "-" in remote_version:
+    v, i, s = remote_version.split("-")
+    remote_version = v + "+" + i + ".git." + s
+
+assert "-" not in remote_version
+assert "." in remote_version
+
+assert os.path.isfile("remote/version.py")
+with open("remote/VERSION", "w", encoding="utf-8") as fh:
+    fh.write(f"{remote_version}\n")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='getBCE',
-    packages=['getBCE'],
-    version='1.5',
+    packages=setuptools.find_packages(),
+    package_data={"getBCE": ["VERSION"]},
+    version=remote_version,
     description='Download data from BCE (Banco Central del ecuador) webpage',
     author='S. Daniel Jaramillo',
     author_email='losteven2018@outlook.com',
