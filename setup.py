dd6df1cb995f0270c4725999d83c752c973154d0
Daniel Jaramillo <steven.villalobos@devsu.com>
2022-04-08 01:17:53 -0500

    refactored setup

--- setup.py
+++ setup.py
@@ -1,38 +1,17 @@
-import setuptools
-import subprocess
-import os
+from distutils.core import setup
 
-remote_version = (
-    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
-    .stdout.decode("utf-8")
-    .strip()
-)
-
-if "-" in remote_version:
-    v, i, s = remote_version.split("-")
-    remote_version = v + "+" + i + ".git." + s
-
-assert "-" not in remote_version
-assert "." in remote_version
-
-assert os.path.isfile("remote/version.py")
-with open("remote/VERSION", "w", encoding="utf-8") as fh:
-    fh.write(f"{remote_version}\n")
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
+setup(
     name='getBCE',
-    packages=setuptools.find_packages(),
+    packages=['getBCE'],
     package_data={"getBCE": ["VERSION"]},
-    version=remote_version,
+    version="1.5.1",
     description='Download data from BCE (Banco Central del ecuador) webpage',
     author='S. Daniel Jaramillo',
     author_email='losteven2018@outlook.com',
     url='https://github.com//Daniejar27/BCE',
     download_url='https://github.com//Daniejar27/BCE',
     keywords=['Economics', 'Banco Central', 'BCE', 'economía'],
+    python_requires=">= 3.8",
     classifiers=[],
-    install_requires=['requests', 'pandas', 'bs4', 'urllib3', 'subprocess', 'setuptools', 'os']
+    install_requires=['requests', 'pandas', 'bs4', 'urllib3']
 )
